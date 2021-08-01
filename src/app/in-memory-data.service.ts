f301d67bd783e213240dc68eea776185ec867e1c
TJ <takajun5252@gmail.com>
2021-08-02 01:09:26 +0900

    テスト用データの変更

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -15,9 +15,9 @@ export class InMemoryDataService implements InMemoryDbService {
       { id: 15, name: '岩手いわ' },
       { id: 16, name: '沖縄縄子' },
       { id: 17, name: '新潟潟子' },
-      { id: 18, name: '織田信長' },
-      { id: 19, name: '豊臣秀吉' },
-      { id: 20, name: '徳川家康' }
+      { id: 18, name: '千葉葉子' },
+      { id: 19, name: '金沢沢子' },
+      { id: 20, name: '山梨梨子' }
     ];
     return {members};
   }
