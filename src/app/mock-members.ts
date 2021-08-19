36eae79de3de9bd7367a513fb4ce5b7eae3f0cf7
TJ <takajun5252@gmail.com>
2021-08-19 16:02:39 +0900

    入社年月日を追加

--- src/app/mock-members.ts
+++ src/app/mock-members.ts
@@ -1,14 +1,14 @@
 import { Member } from './member';
 
 export const MEMBERS: Member[] = [
-  { id: 11, name: 'テスト太郎' },
-  { id: 12, name: 'テスト花子' },
-  { id: 13, name: '山田太郎' },
-  { id: 14, name: '北海道道三郎' },
-  { id: 15, name: '岩手いわ' },
-  { id: 16, name: '沖縄縄子' },
-  { id: 17, name: '新潟潟子' },
-  { id: 18, name: '千葉葉子' },
-  { id: 19, name: '金沢沢子' },
-  { id: 20, name: '山梨梨子' }
+  { id: 11, name: 'テスト太郎', joinDate: '' },
+  { id: 12, name: 'テスト花子', joinDate: ''  },
+  { id: 13, name: '山田太郎', joinDate: ''  },
+  { id: 14, name: '北海道道三郎', joinDate: ''  },
+  { id: 15, name: '岩手いわ', joinDate: ''  },
+  { id: 16, name: '沖縄縄子', joinDate: ''  },
+  { id: 17, name: '新潟潟子', joinDate: ''  },
+  { id: 18, name: '千葉葉子', joinDate: ''  },
+  { id: 19, name: '金沢沢子', joinDate: ''  },
+  { id: 20, name: '山梨梨子', joinDate: ''  }
 ];
